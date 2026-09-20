d2fe5b41a70e91da4194fe1bbd4bf30f3f1256d4
makar <makarenko11223@gamil.com>
2026-09-20 20:49:29 +0000

    solve task

--- task.sh
+++ task.sh
@@ -1,1 +1,26 @@
-#! /bin/bash
+#!/bin/bash
+mysql -u "$DB_USER" -p"$DB_PASSWORD" \
+    -e "CREATE DATABASE ShopDBReserve; 
+        CREATE DATABASE ShopDBDevelopment;"
+
+mysql -u "$DB_USER" -p"$DB_PASSWORD" \
+    -e "USE ShopDBReserve; 
+        CREATE TABLE Products (ID INT AUTO_INCREMENT,Name VARCHAR(50),PRIMARY KEY (ID));
+        USE ShopDBDevelopment; 
+        CREATE TABLE Products (ID INT AUTO_INCREMENT,Name VARCHAR(50),PRIMARY KEY (ID));"
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" \
+    ShopDB \
+    --result-file=backup.sql
+
+
+mysql -u "$DB_USER" -p"$DB_PASSWORD" \
+    ShopDBReserve < backup.sql
+
+
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" \
+    ShopDB \
+    --result-file=backup-no-info-db.sql \
+    --no-create-info
+
+mysql -u "$DB_USER" -p"$DB_PASSWORD" \
+    ShopDBDevelopment < backup-no-info-db.sql
